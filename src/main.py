7e5a99cf2054f3f55a6c5101c09ae8e8246e0be4
silwalanish <silwalanish@gmail.com>
2020-03-09 20:25:20 +0545

    Showcase debug util

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../.env'))
+
+from utils.debug import getLogger
+
+logger = getLogger(__name__)
+
+
 def main():
-    print('Hello, World')
+    logger.info('main: In main {}'.format('Test'))
+    logger.debug('main: 1 + 1 = {}'.format(1 + 1))
+    logger.error('main: 1 + 1 != {}'.format(3))
+    logger.warning('main: The software is still in development')
 
 
 if __name__ == "__main__":
