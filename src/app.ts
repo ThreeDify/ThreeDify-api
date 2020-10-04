db15e050b770dd82f20ea13e42ac7ccbd86183f3
silwalanish <silwalanish@gmail.com>
2020-10-04 20:00:56 +0545

    (hotfix): Disable csrf

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import cors from 'cors';
 import csrf from 'csurf';
 import morgan from 'morgan';
 import sessions from 'express-session';
+import Debug, { Debugger } from 'debug';
 import { json, urlencoded } from 'body-parser';
 import express, { Application } from 'express';
 
 import config from './config';
 import appRouter from './routers';
 import { handle404, handleError } from './errorHandler';
+
+const debug: Debugger = Debug('threedify:app');
 
 const app: Application = express();
 
@@ -16,7 +19,7 @@ app.use(morgan(config.requestLogFormat));
 app.use(cors(config.corsConfig));
 
 app.use(sessions(config.sessionConfig));
-app.use(csrf());
+// app.use(csrf());
 
 app.use(urlencoded({ extended: false }));
 app.use(json());
