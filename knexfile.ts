bd785a10eed9e3d0ce473ee5de4ae58c6284d408
silwalanish <silwalanish@gmail.com>
2020-10-06 22:11:27 +0545

    (chore): Add DB_HOST and DB_PORT to connect to database.

--- knexfile.ts
+++ knexfile.ts
@@ -5,6 +5,8 @@ config();
 const knexConfig: Config = {
   client: 'postgresql',
   connection: {
+    host: process.env.DB_HOST,
+    port: +(process.env.DB_PORT || 5432),
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
