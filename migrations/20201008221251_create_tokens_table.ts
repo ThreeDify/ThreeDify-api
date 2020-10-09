a0f30a4012d1cef6e7c4560b95ad5648374389d3
silwalanish <silwalanish@gmail.com>
2020-10-09 23:15:39 +0545

    (fix): Fix tokens size in tokens migrations.

--- migrations/20201008221251_create_tokens_table.ts
+++ migrations/20201008221251_create_tokens_table.ts
@@ -3,8 +3,8 @@ import * as Knex from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('tokens', (table: Knex.CreateTableBuilder) => {
     table.bigIncrements('id').primary();
-    table.string('access_token').notNullable().unique();
-    table.string('refresh_token').notNullable().unique();
+    table.string('access_token', 2000).notNullable().unique();
+    table.string('refresh_token', 2000).notNullable().unique();
     table.bigInteger('user_id').notNullable();
     table.foreign('user_id').references('users.id');
     table.timestamps(false, true);
