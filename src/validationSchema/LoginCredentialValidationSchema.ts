66bee3b40b3a91bdec7a038df5b36f922e3231d0
silwalanish <silwalanish@gmail.com>
2020-10-10 21:26:50 +0545

    (fix): Only allow alphanumeric and underscore in username

--- src/validationSchema/LoginCredentialValidationSchema.ts
+++ src/validationSchema/LoginCredentialValidationSchema.ts
@@ -2,7 +2,11 @@ import Joi from 'joi';
 
 export const LoginCredentialValidationSchema: Joi.ObjectSchema = Joi.object({
   password: Joi.string().required(),
-  username: Joi.string().alphanum().required(),
+  username: Joi.string()
+    .regex(/^[a-zA-Z0-9_]*$/)
+    .min(5)
+    .max(15)
+    .required(),
 });
 
 export default LoginCredentialValidationSchema;
