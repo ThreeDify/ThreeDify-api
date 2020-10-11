41ba21cbc3e1119726f4d9825ac7fbd4069e472e
silwalanish <silwalanish@gmail.com>
2020-10-11 11:45:39 +0545

    (fix): Create knex connection only once.

--- src/utils/knex.ts
+++ src/utils/knex.ts
@@ -2,8 +2,10 @@ import Knex from 'knex';
 
 import config from '../../knexfile';
 
+const INSTANCE: Knex = Knex(config);
+
 export function knex(): Knex {
-  return Knex(config);
+  return INSTANCE;
 }
 
 export default knex;
