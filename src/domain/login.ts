2a48a123663b6b88aaa889ea20a2439ccbcda0be
silwalanish <silwalanish@gmail.com>
2020-10-18 19:10:34 +0545

    (hot-fix): Preserve JSDocs for interface.

--- src/domain/login.ts
+++ src/domain/login.ts
@@ -10,25 +10,6 @@
  *          type: string
  *        password:
  *          type: string
- *  requestBodies:
- *    LoginCredential:
- *      description: Login credential for user
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/LoginCredential'
- */
-
-export interface LoginCredential {
-  username: string;
-  password: string;
-}
-
-/**
- * @swagger
- *
- * components:
- *  schemas:
  *    TokenCredential:
  *      type: object
  *      properties:
@@ -56,7 +37,19 @@ export interface LoginCredential {
  *        application/json:
  *          schema:
  *            $ref: '#/components/schemas/TokenCredential'
+ *  requestBodies:
+ *    LoginCredential:
+ *      description: Login credential for user
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/LoginCredential'
  */
+
+export interface LoginCredential {
+  username: string;
+  password: string;
+}
 
 export interface TokenCredential {
   accessToken?: string;
