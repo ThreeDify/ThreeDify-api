c9eff21d720856e116b2e0d5b23f3484c926aa23
silwalanish <silwalanish@gmail.com>
2020-10-18 12:46:07 +0545

    (chore): Setup swagger-ui for api docs.

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import homeRouter from './home';
+import docsRouter from './docs';
 import authRouter from './auth';
 import userRouter from './users';
 import imageRouter from './images';
@@ -9,6 +10,7 @@ import reconstructionRouter from './reconstructions';
 const router: Router = Router();
 
 router.use('/', homeRouter);
+router.use('/docs', docsRouter);
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/images', imageRouter);
